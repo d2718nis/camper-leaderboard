aba04ce6ac8d28b1ba0087fb2542cdc031d8fcfb
Denis Zharikov <d2718nis@gmail.com>
2017-05-13 06:35:49 +0300

    Initial commit

---
+++ src/CamperLeaderboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CamperLeaderboard.css';
+
+class CamperLeaderboard extends Component {
+  render() {
+    return (
+      <div className="CamperLeaderboard">
+        <h1>Campler Leaderboard</h1>
+      </div>
+    );
+  }
+}
+
+export default CamperLeaderboard;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CamperLeaderboard from './CamperLeaderboard';
+import './index.css';
+
+ReactDOM.render(
+  <CamperLeaderboard />,
+  document.getElementById('root')
+);
