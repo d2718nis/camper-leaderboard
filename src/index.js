64fa926c9a3eb6097aa1f206763ef241e27de339
Denis Zharikov <d2718nis@gmail.com>
2017-05-14 19:55:19 +0300

    Write async getAjax()

--- src/index.js
+++ src/index.js
@@ -3,7 +3,33 @@ import ReactDOM from 'react-dom';
 import CamperLeaderboard from './CamperLeaderboard';
 import './index.css';
 
-ReactDOM.render(
-  <CamperLeaderboard />,
-  document.getElementById('root')
-);
+getAjax('https://fcctop100.herokuapp.com/api/fccusers/top/recent').then(function(response) {
+  console.log('success');
+  ReactDOM.render(
+    <CamperLeaderboard />,
+    document.getElementById('root')
+  );
+}, function(err) {
+  console.log('Error: ' + err);
+  ReactDOM.render(
+    <CamperLeaderboard />,
+    document.getElementById('root')
+  );
+});
+
+function getAjax(resource) {
+  return new Promise(function(resolve, reject) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          resolve(this.responseText);
+        } else {
+          reject(this.statusText);
+        }
+      }
+    };
+    xhttp.open('GET', resource, true);
+    xhttp.send();
+  });
+}
